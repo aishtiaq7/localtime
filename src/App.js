97a855f8494fd49395cea1ccbb4768ddc52b7741
iMac HOME <imacemail@gmail.com>
2021-08-26 08:20:46 -0700

    added vancovuer local time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [vanTime, setVanTime] = useState();
+
+  const callback = () =>{
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    setVanTime(time)
+  }
+
+  setInterval(callback, 1000);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank" 
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='container'>
+        <div className='singleTime'>
+          <h3>Vancouver time:</h3>
+          <h3>{vanTime}</h3>
+        </div>
+
+        <div className='singleTime'>
+          <h3>Vancouver time:</h3>
+          <h3>{vanTime}</h3>
+        </div>
+      </div>
     </div>
   );
 }
